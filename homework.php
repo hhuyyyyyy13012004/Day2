9a33f35eae400ce495388c3af707c08c1a39514b
hhuyyyyyy13012004 <hoanghuyn98@gmail.com>
2025-07-21 12:58:17 +0700

    change

--- homework.php
+++ homework.php
@@ -1,7 +1,6 @@
 <?php
 if (session_id() === '') session_start();
 
-// Chỉ khai báo class một lần duy nhất
 class Customer {
     public $id, $username, $password, $fullname, $address, $phone, $gender, $birthday;
 
